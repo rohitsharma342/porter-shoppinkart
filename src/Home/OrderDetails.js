49852e5911b2184340788420aa42a752bbdf0b7b
rohitsharma342 <rohitkumar.brainbox@gmail.com>
2024-06-04 12:53:12 +0530

    dsa

--- src/Home/OrderDetails.js
+++ src/Home/OrderDetails.js
@@ -1,127 +1,227 @@
 import { Divider } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
-  };
+  return new URLSearchParams(useLocation().search);
+};
 function OrderDetails() {
-    const query = useQuery();
-  const id = query.get('orderid');
-  const userId = query.get('userid');
-  const [order,setOrder]=useState({})
-  const [user,setUser]=useState({})
+  const query = useQuery();
+  const id = query.get("orderid");
+  const userId = query.get("userid");
+  const [order, setOrder] = useState({});
+  const [user, setUser] = useState({});
 
+  useEffect(() => {
+    fetch(`http://localhost:80/api/users/${userId}`, {
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        // console.log(response)
+        setUser(response.user);
+      });
+    fetch(`http://localhost:80/api/orders/track?id=${id}`, {
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((response) => {
+        // console.log(response)
+        setOrder(response.completeOrder);
+      });
+  }, []);
+  const [delivery_instructions, setdelivery_instructions] = useState({
+    instructions_list: [
+      {
+        type: order?.deliveryInstruction || "cx",
+        description: order?.deliveryInstruction || "cx",
+      },
+    ],
+  });
 
-//   useEffect(()=>{
-//     fetch(`http://localhost:80/api/users/${userId}`,{
-//         headers: { "Content-Type": "application/json" },
-//     }).then((res)=>{return res.json()})
-//     .then((response)=>{
-//         console.log(response)
-//         setUser(response.user)
-//     })
-//     fetch(`http://localhost:80/api/orders/track?id=${id}`,{
-//         headers: { "Content-Type": "application/json"},
-//     }).then((res)=>{return res.json()})
-//     .then((response)=>{
-//         console.log(response)
-//         setOrder(response.completeOrder)
-//     })
-//   },[])
-  const [delivery_instructions,setdelivery_instructions]=useState({
-    instructions_list:[
-        {
-            type: "order.deliveryInstruction",
-            description: "order.deliveryInstruction"
-        }
-    ]
-  })
+  const [pickup_details, setpickup_details] = useState({
+    address: {
+      apartment_address: "Alankar",
+      street_address1: "central spine",
+      street_address2: "two",
+      landmark: "dominos",
+      city: "jaipur",
+      state: "Rajasthan",
+      pincode: "302039",
+      country: "India",
+      lat: 12.9165757,
+      lng: 77.6101163,
+      contact_details: {
+        name: "Rohit",
+        phone_number: "7877575481",
+      },
+    },
+  });
 
-  const [pickup_details,setpickup_details]=useState({
-    address:{
-        apartment_address:"Alankar",
-        street_address1:"central spine",
-        street_address2:"",
-        landmark:"dominos",
-        city:"jaipur",
-        state:"Rajasthan",
-        pincode:"302039",
-        country:"India",
-        lat:342343,
-        lng:32324,
-        contact_details:{
-            name:"Rohit",
-            phone_number:"7877575481"
-        }
-    }
-  })
-
-  const [drop_details,setdrop_details]=useState({
-    address:{
-        apartment_address:"order.houseNo",
-        street_address1:"order.street",
-        street_address2:"",
-        landmark:"order.landmark",
-        city:"order.city",
-        state:"order.state",
-        pincode:"order.pincode",
-        country:"India",
-        lat:"user?.lat",
-        lng:"user?.long",
-        contact_details:{
-            name:"order.companyName",
-            phone_number:"user?.phone"
-        }
-    }
-  })
-const [request_id,setrequest_id]=useState("id")
-function handleRequestPorter(){
+  const [drop_details, setdrop_details] = useState({
+    address: {
+      apartment_address: order?.houseNo || "xc",
+      street_address1: order?.street || "cx",
+      street_address2: "two" || "c",
+      landmark: order?.landmark || "cx",
+      city: order?.city || "cx ",
+      state: order?.state || "cx",
+      pincode: order?.pincode || "cx",
+      country: "India",
+      lat: user?.lat || 12.9165757,
+      lng: user?.long || 77.6101163,
+      contact_details: {
+        name: order?.companyName || "cx",
+        phone_number: user?.phone || "7877575481",
+      },
+    },
+  });
+  const [request_id, setrequest_id] = useState("rohittest0001");
+  const handleRequestPorter = async () => {
     // if(!user.lat || !user.long){
     //     alert("hello enter your lat long")
     // }else{
-        const formData ={request_id,delivery_instructions,pickup_details,drop_details}
-        fetch(`https://pfe-apigw-uat.porter.in/v1/orders/create`,{
-            mode: 'no-cors',
-            method:"POST",
-            headers: { "x-api-key":"d8635c60-09b4-4c44-828f-d2da5bf56c79","Content-Type": "application/json" },
-            body:JSON.stringify(formData)
-        }).then((res)=>{return res.json()})
-        .then((response)=>{
-            console.log(response)
-           
-        })
-    // }
-}
-    return ( 
-        <>
-        <h1 className="ms-10 mt-10">Order Details:</h1>
+    const requestBody = {
+      request_id: "Shopinkarts_order_00004",
+      delivery_instructions: {
+        instructions_list: [
+          {
+            type: "text",
+            description: "handle with care",
+          },
+          {
+            type: "text",
+            description: "Test order 52",
+          },
+        ],
+      },
+      pickup_details: {
+        address: {
+          apartment_address: "27",
+          street_address1: "Sona Towers",
+          street_address2: "Krishna Nagar Industrial Area",
+          landmark: "Hosur Road",
+          city: "Bengaluru",
+          state: "Karnataka",
+          pincode: "560029",
+          country: "India",
+          lat: 12.939391726766775,
+          lng: 77.62629462844717,
+          contact_details: {
+            name: "Porter Test User",
+            phone_number: "+911234567890",
+          },
+        },
+      },
+      drop_details: {
+        address: {
+          apartment_address: "this is apartment address",
+          street_address1: "BTM Layout",
+          street_address2: "Another street address",
+          landmark: "BTM Layout",
+          city: "Bengaluru",
+          state: "Karnataka",
+          pincode: "560029",
+          country: "India",
+          lat: 12.9165757,
+          lng: 77.6101163,
+          contact_details: {
+            name: "Porter Test User",
+            phone_number: "+911234567890",
+          },
+        },
+      },
+    };
 
-        <div className="flex ms-10 mt-10">
-            
-          <div className="flex justify-around space-x-20">
-            <div>
-                <h1><b>ORDER ID</b> : {id}</h1>
-            </div>
-            <div>
-                <h1><b>ORDER STATUS</b> : {"order?.trackingDetails?.title"}</h1>
-            </div>
-            <div>
-                <h1><b>USER NAME</b> : {"order?.companyName"}</h1>
-            </div>
-            <div>
-                <h1><b>USER PHONE</b> : {"user?.phone"}</h1>
-            </div>
-            <div>
-                <h1><b>AMOUNT</b> : {"order?.totalAmount"}</h1>
-            </div>
-            <div>
-               <button className=" bg-blue-700 px-3 py-2 mb-10 text-white" onClick={handleRequestPorter}>REQ on Porter</button>
-            </div>
+    // const formData ={request_id,delivery_instructions,pickup_details,drop_details}
+    console.log(requestBody);
+    try {
+      const response = await fetch(
+        "http://localhost:80/proxy/v1/orders/create",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": "d8635c60-09b4-4c44-828f-d2da5bf56c79"
+          },
+          body: JSON.stringify(requestBody),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+
+      const data = await response.json();
+       console.log(data.order_id)
+      fetch(
+        `http://localhost:80/api/porter/request/${id}?porterId=${data.order_id}`,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((response) => {
+          // console.log(response)
+          // setOrder(response.completeOrder)
+        });
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
+  // }
+
+  return (
+    <>
+      <h1 className="ms-10 mt-10">Order Details:</h1>
+
+      <div className="flex ms-10 mt-10">
+        <div className="flex justify-around space-x-20">
+          <div>
+            <h1>
+              <b>ORDER ID</b> : {id}
+            </h1>
           </div>
-          <Divider/>
+          <div>
+            <h1>
+              <b>ORDER STATUS</b> : {order?.trackingDetails?.title}
+            </h1>
+          </div>
+          <div>
+            <h1>
+              <b>USER NAME</b> : {order?.companyName}
+            </h1>
+          </div>
+          <div>
+            <h1>
+              <b>USER PHONE</b> : {user?.phone}
+            </h1>
+          </div>
+          <div>
+            <h1>
+              <b>AMOUNT</b> : {order?.totalAmount}
+            </h1>
+          </div>
+          {order.orderStatus === 0 && (
+            <div>
+              <button
+                className=" bg-blue-700 px-3 py-2 mb-10 text-white"
+                onClick={handleRequestPorter}
+              >
+                REQ on Porter
+              </button>
+            </div>
+          )}
         </div>
-        </>
-     );
+        <Divider />
+      </div>
+    </>
+  );
 }
 
 export default OrderDetails;
--- src/Home/PorterStatus.js
+++ src/Home/PorterStatus.js
@@ -1,12 +1,55 @@
+import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
+const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
 function PorterStatus() {
+    const query = useQuery();
+    const id = query.get('orderid');
+    const userId = query.get('userid');
+    const [porter,setPorter]=useState(null)
+    useEffect(()=>{
+        fetch(`http://localhost:80/api/porter/get/${id}`,{
+            headers: { "Content-Type": "application/json" },
+        }).then((res)=>{return res.json()})
+        .then((response)=>{
+           if(response.status==true){
+            setPorter(response.data)
+            console.log(response)
+           }else{
+            setPorter(null)
+           }
+        })
+        try {
+            const response =  fetch('http://localhost:80/proxy/v1/orders/CRN1716800105713', {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json"
+              }
+            //   body: JSON.stringify(formData)
+            });
+      
+            if (!response.ok) {
+              throw new Error('Network response was not ok ' + response.statusText);
+            }
+      
+            const data =  response.json();
+            // console.log(data.order_id)
+         
+          } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+          }
+      },[])
+
     return ( 
         <>
-        <div className="text-center">
+        <div className="text-center mt-5">
             <h1 className="text-2xl font-bold">Porter Status</h1>
         </div>
+        {porter!==null &&
         <div className="w-full text-center mt-10">
         <div>
-            <p className="text-2xl font-bold">CRN1716800105713</p>
+            <p className="text-2xl font-bold">{porter.porterId}</p>
             <p>pickup_time : 12345</p>
             <p>order_accepted_time : 12345</p>
             <p>order_started_time : 12345</p>
@@ -17,6 +60,7 @@ function PorterStatus() {
            <button className="bg-red-600 px-4 mt-5 text-white py-1">Cancle Now</button>
         </div>
         </div>
+}
         </>
      );
 }
